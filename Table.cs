2e9401abe7aaa7fe751e181b5c2e969dc2318b9a
mclh10 <56630236+mclh10@users.noreply.github.com>
2019-12-11 15:52:31 +0100

    Fichiers classes

---
+++ Table.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Table : Materiel
+    {
+        private int hauteur;
+        private int nbPieds;
+
+        public int Hauteur
+        {
+            get => hauteur;
+            set => hauteur = value;
+        }
+
+        public int NbPieds
+        {
+            get => nbPieds;
+            set => nbPieds = value;
+        }
+
+
+        public override void SeDecrire()
+        {
+
+        }
+    }
+}
